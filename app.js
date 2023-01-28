0ed176abaf43b8efa053c947ae3d4ad6829ef38e
KilicEness <eneskiliicc06@gmail.com>
2023-01-28 17:32:19 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//Middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} de başlatıldı.`); //bactick option + , ile yapıldı.
+});
