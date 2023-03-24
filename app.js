901efd045a59ddcd9cb07e25395f2b33a203ea92
KilicEness <eneskiliicc06@gmail.com>
2023-03-24 15:23:44 +0300

    template engine and operations

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-//Middlewares
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MİDDLEWARES
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
